8a173f4168f559a0a93dbc92bac0377c899f863d
Danielle Dempsey <ddempsey@perennia.ca>
2023-04-19 12:52:06 -0300

    move data import and plot params to R file
    move plot code to summaryplots package

---
+++ pages/import_data_and_params.R
@@ -0,0 +1,66 @@
+library(dplyr)
+library(DT)
+library(here)
+library(ggplot2)
+library(leaflet)
+library(lubridate)
+library(plotly)
+library(RColorBrewer)
+library(readr)
+library(summaryplots)
+library(stringr)
+library(tidyr)
+
+
+
+# datatable settings ------------------------------------------------------
+
+dt_options <- list(
+  dom = 'ft',
+  paging = FALSE,
+  searching = TRUE,
+  scrollY = "550px",
+  pageLength = 500,
+  columnDefs = list(list(className = 'dt-center', targets = "_all"))
+)
+
+
+# data --------------------------------------------------------------------
+
+# summarized data - all observations
+dat <- read_csv(
+  here("pages/data/summary.csv"), show_col_types = FALSE
+)
+
+# summarized data - filtered
+dat_filt <- read_csv(
+  here("pages/data/summary_filtered_data.csv"), show_col_types = FALSE
+) %>% 
+  filter(variable == "Temperature")
+
+# gross range thresholds (sensors)
+sensors <- read_csv(
+  here("pages/data/sensors.csv"), show_col_types = FALSE
+)
+
+# station locations
+st_locations <- read_csv(
+  here("pages/data/Station_Locations_2022-12-06.csv"), show_col_types = FALSE
+) %>% 
+  mutate(
+    STATION = case_when(
+      STATION == "Sandy Cove St. Mary's" ~ "Sandy Cove St. Marys",
+      STATION == "Larry's River" ~ "Larrys River",
+      TRUE ~ STATION),
+    popup = paste("County: ", COUNTY, "</br>", "Station: ", STATION) 
+  )
+
+# DO units for each station
+do_units <- read_csv(here("pages/data/do_units.csv"), show_col_types = FALSE)
+
+# plot settings -----------------------------------------------------------
+
+theme_set(theme_light())
+
+county_pal <- get_county_colour_palette(length(unique(st_locations$COUNTY)))
+
